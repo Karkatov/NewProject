f4d8668764ed7497b222f1e706fead6b822f154f
duxxless <karkatof@ya.ru>
2022-09-16 19:43:22 +0300

    Added a new package of "Bank"

---
+++ src/com/javastart/bankhomework/entity/Payment.java
@@ -0,0 +1,17 @@
+package com.javastart.bankhomework.entity;
+
+public class Payment {
+    private Bill bill;
+
+    public Bill getBill() {
+        return bill;
+    }
+
+    public void setBill(Bill bill) {
+        this.bill = bill;
+    }
+
+    public Payment(Bill bill) {
+        this.bill = bill;
+    }
+}
